1361cdafb270557cda60c6998b4ba9a3a66d8f9e
Xavier <xmtrinidad@gmail.com>
2024-01-17 07:08:04 -0700

    messing with pointers

---
+++ pointers/pointers.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	age := 32
+
+	var agePointer *int
+	agePointer = &age
+
+	fmt.Println("Age:", *agePointer)
+
+	editAgeToAdultYears(agePointer)
+	fmt.Println(age)
+}
+
+func editAgeToAdultYears(age *int) {
+	// return *age - 18
+	*age = *age - 18
+}
